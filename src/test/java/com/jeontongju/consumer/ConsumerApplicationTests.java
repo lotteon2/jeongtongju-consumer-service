dffb28bd009c76cdd9d03c630226dc235873bf88
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-11-19 16:09:04 +0900

    first commit

---
+++ src/test/java/com/jeontongju/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeontongju.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
