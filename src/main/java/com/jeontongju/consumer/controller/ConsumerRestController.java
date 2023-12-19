c41594a617e07fe3c134205855ae5b2b8230d6b1
woo yup Lee <zjadlspun1114@naver.com>
2023-12-19 11:56:23 +0900

    FEAT 소비자 개인정보 조회 로직 작성 SCRUM-66

--- src/main/java/com/jeontongju/consumer/controller/ConsumerRestController.java
+++ src/main/java/com/jeontongju/consumer/controller/ConsumerRestController.java
@@ -1,8 +1,14 @@
 package com.jeontongju.consumer.controller;
 
+import com.jeontongju.consumer.dto.response.ConsumerInfoForInquiryResponseDto;
 import com.jeontongju.consumer.service.ConsumerService;
+import io.github.bitbox.bitbox.dto.ResponseFormat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConsumerRestController {
 
   private final ConsumerService consumerService;
+
+  @GetMapping("/consumers")
+  public ResponseEntity<ResponseFormat<ConsumerInfoForInquiryResponseDto>> getMyInfo(
+      @RequestHeader Long memberId) {
+
+    ConsumerInfoForInquiryResponseDto myInfoResponseDto = consumerService.getMyInfo(memberId);
+    return ResponseEntity.ok()
+        .body(
+            ResponseFormat.<ConsumerInfoForInquiryResponseDto>builder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.name())
+                .detail("소비자 개인정보 조회 성공")
+                .data(myInfoResponseDto)
+                .build());
+  }
 }
---
+++ src/main/java/com/jeontongju/consumer/dto/response/ConsumerInfoForInquiryResponseDto.java
@@ -0,0 +1,18 @@
+package com.jeontongju.consumer.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class ConsumerInfoForInquiryResponseDto {
+
+  private String email;
+  private String name;
+  private String phoneNumber;
+  private String profileImageUrl;
+}
